3e02ae38ab2a9d76b974a53a5a35e8044b95e976
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-05-23 20:15:39 +0900

    4번과 구구단

--- src/section05/LoopHomework.java
+++ src/section05/LoopHomework.java
@@ -21,14 +21,59 @@ public class LoopHomework {
 //		}
 		
 		/*3번*/
+//		for(int i=4; i>0; i--) {
+//			for(int j=1; j<8; j++) {
+//				if(8-i<j||j<i) System.out.print(" ");
+//				else System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 		
-		for(int i=4; i>0; i--) {
-			for(int j=1; j<8; j++) {
-				if(8-i<j||j<i) System.out.print(" ");
-				else System.out.print("*");
+		/*4번*/
+//		for(int i=0; i<7; i++) {
+//			for(int j=0; j<7; j++) {
+//				if(i<4) {
+//					if(i+j<3||j-i>3) System.out.print(" ");
+//					else System.out.print("*");
+//				} else {
+//					if(i-j>3||i+j>9) System.out.print(" ");
+//					else System.out.print("*");
+//				}
+//				
+//			}
+//			System.out.println();
+//		}
+		
+		
+		/*
+		 *    *   
+		 	 ***  
+		 	***** 
+		   *******
+		    ****** i=4, j=6
+		     ***** i=5, j=5,6
+		      **** i=6, j=4,5,6
+		 * 
+		 * 
+		 * 
+		 */
+		
+		/*구구단*/
+		for(int x=0; x<3; x++) {
+			for(int i=1; i<10; i++) {
+				for(int j=2; j<5; j++) {
+					//System.out.print(j + " * " + i + " = " + j*i + "\t");
+					int dan = x*3+j;
+					if(dan>9) break;
+					System.out.printf("%d * %d = %d\t", dan, i, dan*i);
+					
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
+		
+		
+		
 		
 		
 	}
