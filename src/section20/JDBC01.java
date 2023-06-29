3f1f9503c135bf485ca04c08b55d75834a9f0b71
easyO <geostarboy@gmail.com>
2023-06-29 21:52:15 +0900

    DB

--- src/section20/JDBC01.java
+++ src/section20/JDBC01.java
@@ -1,5 +1,15 @@
 package section20;
-
+/*
+ * JDBC(JAVA DataBase Connectivity)
+ * 	자바 어플리케이션에서 데이터 베이스와 통신할 수 있는 API
+ * 
+ * JDBC 설정
+ * 	해당 DBMS 관련 jdbc.jar 추가해야한다.
+ * 	ex) 오라클 -> ojdbc8.jar
+ * 		[Build path] -> [Configure Build path ...] -> [Libraries] 탭
+ * 		-> [Add External JARs..] 경로로 ojdbc8.jar 파일 추가
+ * 
+ */
 public class JDBC01 {
 
 }
